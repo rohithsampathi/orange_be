d52b610e5bc37e48e613b8eb79d995efdd928c8b
Rohith Sampathi <skyguardian@Rohiths-MacBook-Pro.local>
2024-07-04 23:18:58 +0530

    v2.7 users added

--- utils/config.py
+++ utils/config.py
@@ -8,5 +8,8 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 # This is a simple in-memory user store. In a real application, you'd use a database.
 users_db = {
-    "testuser": {"username": "testuser", "password": "testpass"}
+    "testuser": {"username": "testuser", "password": "testpass"},
+    "kavya@montaigne.co": {"username": "kavya@montaigne.co", "password": "kavya@msbs"},
+    "media@montaigne.co": {"username": "media@montaigne.co", "password": "media@msbs"},
+    "rohith@montaigne.co": {"username": "rohith@montaigne.co", "password": "rohith@msbs"}
 }
